f1a4f62af458c79e7dd87d2e951a3e397c8be45e
LunexINVOKER <karapuz2209@gmail.com>
2026-04-01 14:07:26 -0400

    add some icons

---
+++ client/public/sw.js
@@ -0,0 +1,14 @@
+const CACHE = "gymlog-v1";
+const ASSETS = ["/", "/index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
